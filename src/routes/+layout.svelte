5320689b2de0285cb3ac052b6df6cba917a76f17
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2024-11-13 00:39:47 +0530

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	// global styles
+	import "../styles.scss";
+
+	let { children } = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style lang="scss">
+	main {
+		padding: 5px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Welcome to my website!</title>
+</svelte:head>
+
+<main>
+	<h1>Work in Progress</h1>
+	<p>Hi, I'm still thinking what to do with this website. Check back later!</p>
+</main>
+
+<style lang="scss">
+	h1,
+	p {
+		padding: 0;
+		margin: 0;
+	}
+</style>
